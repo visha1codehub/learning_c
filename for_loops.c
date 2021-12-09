1a36c68e09869486f01e507755fe42684ce3d852
Vishal <itsvishalhack@gmail.com>
2021-12-09 22:44:36 +0530

    again start learning C

--- for_loops.c
+++ for_loops.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 int main()
-{   int i,a;
+{
+    int i, a, j;
     char b;
 
     // printf("Enter your number : ");
-    // scanf("%d",&a);
-    // printf("Table of %d :\n",a);
+    // scanf("%d", &a);
+    // printf("Table of %d :\n", a);
 
-    // for (i = 1; i <= 10; i+=1){
-    //     printf("%d\n",i*a);
+    // for (i = 1; i <= 10; i += 1)
+    // {
+    //     printf("%d\n", i * a);
     // }
-    for ( i = 1; i < 5; i++)
+    for (i = 1; i <= 5; i++)
     {
-        printf("%c",i);
+        for (j = 1; j <= i; j++)           
+        {
+            printf("* ");
+        }
+        printf("\n");
     }
-    
-    
+
     return 0;
 }
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
-{
+int main()
+{  
     printf("Hello World! This is very confusing. I am so Confused.\n");
     return 0;
 }
